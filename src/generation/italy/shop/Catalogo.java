170d03826baefc0af833bafe1fa466b54d7de5b1
masfreddy93 <masieronicola@gmail.com>
2022-11-18 16:34:19 +0100

    third part implemented

---
+++ src/generation/italy/shop/Catalogo.java
@@ -0,0 +1,62 @@
+package generation.italy.shop;
+
+import java.util.Scanner;
+
+public class Catalogo {
+
+	public static void main(String[] args) {
+		
+//		Televisore t = new Televisore(72621, "tv", "samsung", 350, 20, 250, 170, 30, true);
+//		String res = t.toString();
+//		System.out.println("Televisore 1: " + res);
+//		
+		Scanner sc = new Scanner(System.in);
+		
+//		System.out.println("-------------");
+//		System.out.println("inserire dati per televisore");
+//		Televisore t2 = new Televisore(sc.nextInt(), sc.nextLine(), sc.nextLine(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), true);
+//		res = t2.toString();
+//		System.out.println("Televisore 2: " + res);
+		
+//		System.out.println("-------------");
+//		System.out.println("inserire dati per televisore");
+//		Prodotto p = new Televisore(sc.nextInt(), sc.nextLine(), sc.nextLine(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), true);
+//		res = p.toString();
+//		System.out.println("Televisore 2: " + res);
+		
+		System.out.println("Catalogo prodotti");
+		System.out.println("Cosa stai inserendo? Digita 1 per 'Smartphone', digita 2 per 'Televisore' o digita 3 per 'Cuffie'");
+		int numScelto = sc.nextInt();
+		System.out.println("Numero digitato: " + numScelto);
+		
+		if(numScelto == 1 || numScelto == 2 || numScelto == 3) {
+			System.out.println("Numero valido");
+		} else {
+			System.out.println("Hai inserito un numero non valido");
+		}
+		
+		
+		System.out.println("-------------");
+
+		if(numScelto == 1) {
+			System.out.println("Inserire dati per smartphone");
+			Prodotto p = new Smartphone(sc.nextInt(), sc.nextLine(), sc.nextLine(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+			String res = p.toString();
+			System.out.println("Smartphone: " + res);
+		}else if(numScelto == 2) {
+			System.out.println("Inserire dati per televisore");
+			Prodotto p= new Televisore(sc.nextInt(), sc.nextLine(), sc.nextLine(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextBoolean());
+			String res = p.toString();
+			System.out.println("Smartphone: " + res);
+		}else {
+			System.out.println("Inserire dati per cuffie");
+			Prodotto p = new Cuffie(sc.nextInt(), sc.nextLine(), sc.nextLine(), sc.nextInt(), sc.nextInt(), sc.nextLine(), sc.nextBoolean());
+			String res = p.toString();
+			System.out.println("Smartphone: " + res);
+		}
+		
+		sc.close();
+		
+		
+	}
+}
