2d29c7879d1b3c34dfae8b309483ecf7c70c966f
masfreddy93 <masieronicola@gmail.com>
2022-11-18 15:10:31 +0100

    first part done

--- src/generation/italy/shop/Main.java
+++ src/generation/italy/shop/Main.java
@@ -4,6 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("ciao");
+		Prodotto p = new Prodotto(23782, "Pallina", "Nike", 12, 10);
+		p.setIva(20);
+		
+		
+		int priceBase = p.getPrezzo();
+		System.out.println("Prezzo base: " + priceBase); //restituisce int prezzo
+		
+		float priceWithIva = p.getPriceWithIva();
+		System.out.printf("Prezzo con iva: %.2f", priceWithIva);
+		
+		String res = p.toString();
+		System.out.println("\n------");
+		System.out.println("\nProdotto" + res); //restituisce oggetto
 	}
 }
--- src/generation/italy/shop/Prodotto.java
+++ src/generation/italy/shop/Prodotto.java
@@ -2,5 +2,87 @@ package generation.italy.shop;
 
 public class Prodotto {
 	
+	//variabili d'istanza
+	private int codice;
+	private String nome;
+	private String marca;
+	private int prezzo;
+	private int iva;
 	
+	
+	//costruttore
+	public Prodotto(int codice, String nome, String marca, int prezzo, int iva) {
+		
+		this.codice = codice;
+		this.nome = nome;
+		this.marca = marca;
+		this.prezzo = prezzo;
+		this.iva = iva;
+	}
+	
+	//getter/setter
+	public int getCodice() {
+		
+		return this.codice;
+	}
+	
+	public String getNome() {
+		
+		return this.nome;
+	}
+	public void setNome(String nome) {
+		
+		this.nome = nome;
+	}
+	
+	public String getMarca() {
+		
+		return this.marca;
+	}
+	public void setMarca(String marca) {
+		
+		this.marca = marca;
+	}
+	
+	public int getPrezzo() {
+		
+		return this.prezzo;
+	}
+	public void setPrezzo(int prezzo) {
+		
+		if(prezzo > 0)
+			this.prezzo = prezzo;
+	}
+	
+	public int getIva() {
+		
+		return this.iva;
+	}
+	public void setIva(int iva) {
+		
+		if(iva >= 0)
+			this.iva = iva;
+	}
+	
+	
+	//altri metodi
+	
+	public float getPriceWithIva() {
+		
+		float priceWithIva = this.prezzo + (this.prezzo / 100f * this.iva);
+				
+		return priceWithIva;
+	}
+	
+	
+	//toString
+	@Override
+	public String toString() {
+		
+		return "\nCodice: " + codice +
+				"\nNome: " + nome +
+				"\nMarca: " + marca +
+				"\nPrezzo: " + prezzo +
+				"\nIva: " + iva;
+	}
 }
